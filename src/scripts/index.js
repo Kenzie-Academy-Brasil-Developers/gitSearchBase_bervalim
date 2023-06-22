c3429751dcc45b743fa0d7b0c278adc1680a14d2
bervalim <bernardogvalim@gmail.com>
2023-06-21 21:32:29 -0300

    Entrega gitSearch

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,16 @@ import { searchGitHubUser, searchGitHubRepositorie } from "./requests.js";
 function handleSearchGitHubUser() {
   const inputGitHubUser = document.querySelector(".index__input");
   const buttonGitHubUser = document.querySelector(".index__button");
+  const spinner = document.querySelector(".spinner");
 
   buttonGitHubUser.addEventListener("click", () => {
-    const userGitHubName = inputGitHubUser.value;
-    localStorage.setItem("userName", userGitHubName);
-    searchGitHubUser(userGitHubName);
+    spinner.classList.remove("hidden");
+    setTimeout(() => {
+      const userGitHubName = inputGitHubUser.value;
+      localStorage.setItem("userName", userGitHubName);
+      searchGitHubUser(userGitHubName);
+      spinner.classList.add("hidden");
+    }, 2000);
   });
 }
 handleSearchGitHubUser();
